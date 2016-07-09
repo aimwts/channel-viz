09f0e1061de64ca19cf3fe7ddd36a908449f94eb
aimwts <aimwts@gmail.com>
2016-07-09 15:22:26 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,7 +16,7 @@
 		defaultFeeds	= [345513042], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Taas IOT for Elec.Imp Sensor', // Replaces Xively logo in the header
 		dataDuration	= '365days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 21600, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
